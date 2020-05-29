17dd0d8b2df5acefd9c24b85c01a74638b1ead03
shmcao <shmcao@pku.edu.cn>
2020-05-29 18:25:23 +0800

    initial array generator

---
+++ initial-particles.py
@@ -0,0 +1,21 @@
+import numpy as np
+N = 1000000
+D = 2  # dimension
+ppt = 2  # property number
+result = np.zeros([D * 2 + ppt, N])
+# np.random.rand() gives random number in [0,1)
+for i in range(D):
+    # X and Y columns
+    result[i] = (np.random.rand(N)-0.5)*200
+for i in range(D):
+    # Vx and Vy columns
+    result[D + i] = np.random.rand(N)-0.5
+for i in range(ppt):
+    if (i == 0):
+        # mass
+        result[D * 2 + i] = abs(np.random.randn(N))
+    if (i == 1):
+        # flag
+        result[D * 2 + i] = 1
+print(result.shape)
+np.savetxt('initial-1000000', result.T, delimiter='\t')
